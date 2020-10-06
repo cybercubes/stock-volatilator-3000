2cc3d350bc33524b7dfe6c1c3dc7e587efa21a53
Kirill Lossenko <kiloss@ttu.ee>
2020-10-06 15:30:28 +0300

    added VolatilityControllerTest.java and made 2 simple tests

--- src/test/java/ee/taltech/volatilator/HttpRequestTest.java
+++ src/test/java/ee/taltech/volatilator/HttpRequestTest.java
@@ -1,32 +1,38 @@
 package ee.taltech.volatilator;
 
+import ee.taltech.volatilator.dto.VolatilityResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void greetingShouldReturnDefaultMessage() {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("API is up");
+        assertThat(this.restTemplate.getForObject("/", String.class)).isEqualTo("API is up");
     }
 
     @Test
     public void NonExistingMappingShouldReturnNotFoundMessage() {
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/nonExistingMapping",
+        assertThat(this.restTemplate.getForObject( "/nonExistingMapping",
                 String.class)).contains("error-404");
+    }
+
+    @Test
+    public void CorrectMappingReturnsJson() {
+        VolatilityResponse forObject = this.restTemplate.getForObject("/volatilator",
+                VolatilityResponse.class);
+
+
+
+        //assertThat(forObject).contains("error-404");
     }
 
 }
