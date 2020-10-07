8d14878f4e20b04bc6a1cdbe62054c8bc1cde83d
Kirill Lossenko <kiloss@ttu.ee>
2020-10-07 17:47:52 +0000

    Update Tester1.java

--- src/main/java/a_theory/question6/Tester1.java
+++ src/main/java/a_theory/question6/Tester1.java
@@ -1,13 +1,18 @@
 package a_theory.question6;
 
 public class Tester1 {
-
+    //(Lysenko kirill)
     //todo this part is done by only 1 team member
     // make sure to commit under your user otherwise points won't count
     // explain 2 terms from ListOfOptions.java, each team member has to have unique definitions
 
     //todo p1
     // First. Write the definition and explain what is it.
+    //Acceptence testing - Is when the code has passed all the unit tests, integration tests, and
+    //system tests and started being tested as if an average consumer would have used it, to
+    //confirm if the end product matches to what the end user would have anticipated.
+
+    //this type of testing is mostly conducted manually 
 
     //todo p2
     // Second. Write the definition and explain what is it.
