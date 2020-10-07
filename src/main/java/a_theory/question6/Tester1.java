9b985e5eb81464f465be730a3099f9a4c0c122bf
Kirill Lossenko <kiloss@ttu.ee>
2020-10-07 18:12:08 +0000

    Answered 2 questions in question no. 6

--- src/main/java/a_theory/question6/Tester1.java
+++ src/main/java/a_theory/question6/Tester1.java
@@ -10,10 +10,15 @@ public class Tester1 {
     // First. Write the definition and explain what is it.
     //Acceptence testing - Is when the code has passed all the unit tests, integration tests, and
     //system tests and started being tested as if an average consumer would have used it, to
-    //confirm if the end product matches to what the end user would have anticipated.
+    //confirm if the end product matches users expectations.
 
     //this type of testing is mostly conducted manually 
 
     //todo p2
     // Second. Write the definition and explain what is it.
+    //Security testing - Is when the created system is tested for it's potential misuse or shut down. 
+    //These kinds of tests require analysis for possible vectors which would later become test cases.
+    //For example one of potential cases could be unsanitised input fields, so tests for input security must be
+    //implemented.
+
 }
