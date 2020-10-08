04774b2acc83e010049059b808d251e49d4f8de2
Giorgi Zeikidze <gizeik@ttu.ee>
2020-10-08 12:40:49 +0300

    added caching

--- src/main/java/ee/taltech/volatilator/controller/VolatilityController.java
+++ src/main/java/ee/taltech/volatilator/controller/VolatilityController.java
@@ -8,6 +8,8 @@ import ee.taltech.volatilator.responses.SuccessResponse;
 import ee.taltech.volatilator.service.VolatilityService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.format.DateTimeParseException;
@@ -16,10 +18,12 @@ import java.util.EmptyStackException;
 @RequestMapping("/volatilator")
 @RestController
 @Slf4j
+@Component
 public class VolatilityController {
     @Autowired
     private VolatilityService volatilityService;
     @GetMapping()
+    @Cacheable(value = "responses", key = "#symbol.concat(#startDate).concat(#endDate)")
     public Response<?> getData(@RequestParam(defaultValue = "IBM") String symbol,
                                             @RequestParam(defaultValue = "2020-09-06")String startDate,
                                             @RequestParam(defaultValue = "2020-10-03")String endDate ){
