e761b55e5638c2cd40252830d226a3f5104ed0fa
Kirill Lossenko <kiloss@ttu.ee>
2020-10-08 08:35:28 +0000

    answered question no. 1

--- src/main/java/a_theory/question1/TDD.java
+++ src/main/java/a_theory/question1/TDD.java
@@ -8,21 +8,23 @@ public class TDD {
     //todo p1
     // Description above would work for a metal band.
     // What does TDD stand for in IT?
-    // Answer:
+    // Answer: Test Driven Development
 
     //todo p2
     // Can you describe 3 rules of TDD? In your words (no wiki definitions)
-    // 1
-    // 2
-    // 3
+    // 1 You are only allowed to write production code if your inteniton is to make a failing test pass 
+    // 2 You are only allowed to write a unit test that initially fails.
+    // 3 You are only allowed to write production code that makes a failing test pass.
 
     //todo p3
     // Who popularized the concept of TDD?
     // Answer:
+    //Kent Beck
 
     //todo p4
     // Can you name their most famous book?
     // Answer:
+    // Test Driven Development: by example
 
 
 }
