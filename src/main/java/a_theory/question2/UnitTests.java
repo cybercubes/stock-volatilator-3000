cfe70fb0471e141f5e6ce08c6dfe4db5347ef10c
Kirill Lossenko <kiloss@ttu.ee>
2020-10-08 08:56:53 +0000

    Answerd Question no. 2

--- src/main/java/a_theory/question2/UnitTests.java
+++ src/main/java/a_theory/question2/UnitTests.java
@@ -4,24 +4,27 @@ public class UnitTests {
 
     //todo p1
     // What are unit tests? In your words (don't use wiki definitions)
-    // Answer:
+    // Answer: unit tests are there to test a specific function or a class. 
 
     //todo p2
     // Name 2 benefits of unit tests
-    // 1
-    // 2
+    // 1 They promote the Agile approach which allows for better scalability
+    // 2 They are fast
 
     //todo p3
     // Name 1 disadvantage of unit tests
-    // 1
+    // 1 the test results do not reflect how the system iteslf is going to function, only it only reflects the 
+    // functionality of it's separate parts
 
     //todo p4
     // Name 2 unit test frameworks or libraries
-    // 1
-    // 2
+    // 1 JUnit
+    // 2 Selenium
 
     //todo p5
     // Would you use mocking with unit tests?
-    // Yes/No:
-    // Why?
+    // Yes/No: Yes
+    // Why? Because mocking a structure that the tested class/function uses would save a significant   
+    // amount of time, which is ideal when you can potentially have hundreds of unit tests that are required to be
+    // ran in succession.
 }
