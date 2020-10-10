a08197dd77f0dec1a2fad0a6272fe688a4505fdc
Giorgi Zeikidze <gizeik@ttu.ee>
2020-10-10 21:35:08 +0300

    Add Integration Tests

--- src/test/java/ee/taltech/volatilator/controller/VolatilityControllerTestDate.java
+++ src/test/java/ee/taltech/volatilator/controller/VolatilityControllerTestDate.java
@@ -1,25 +1,24 @@
 package ee.taltech.volatilator.controller;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.LinkedHashMap;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class VolatilityControllerIntegrationTest {
+class VolatilityControllerTestDate {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    void getDataDefault() {
-        assertEquals(7.904, ((LinkedHashMap) this.restTemplate.getForObject("/volatilator?symbol=FB", LinkedHashMap.class).get("body")).get("volatility"));
+    void dateErrorTest() {
+        String url = "/volatilator?endDate=2040-10-03&startDate=2090-09-06&symbol=IBM";
+        assertThat(((LinkedHashMap) this.restTemplate.getForObject(url, LinkedHashMap.class).get("body")).get("message")).isEqualTo("Provided endDate cannot be in the future.");
     }
-
 }
