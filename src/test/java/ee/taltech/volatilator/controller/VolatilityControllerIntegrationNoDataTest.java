c17265ee63074f435119a2147bef52b5572fe3b5
Giorgi Zeikidze <gizeik@ttu.ee>
2020-10-10 23:55:14 +0300

    cleanup

--- src/test/java/ee/taltech/volatilator/controller/VolatilityControllerIntegrationNoDataTest.java
+++ src/test/java/ee/taltech/volatilator/controller/VolatilityControllerIntegrationNoDataTest.java
@@ -5,12 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.LinkedHashMap;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class VolatilityControllerIntegrationNoDataTest {
@@ -20,7 +18,6 @@ class VolatilityControllerIntegrationNoDataTest {
 
     @Test
     void getDataDefault() {
-//        assertEquals(7.904, ((LinkedHashMap)this.restTemplate.getForObject(, LinkedHashMap.class).get("body")).get("volatility"));
         assertThat(((LinkedHashMap) this.restTemplate.getForObject("/volatilator?endDate=1851-10-03&startDate=1851-09-06&symbol=FB", LinkedHashMap.class).get("body")).get("message")).isEqualTo("No data entries found.");
 
     }
