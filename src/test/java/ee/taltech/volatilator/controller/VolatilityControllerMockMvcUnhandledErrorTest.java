98a492935975ddee88d96b1229e95f302455b5e9
Kirill Lossenko <kiloss@ttu.ee>
2020-10-10 17:23:43 +0300

    renamed a test

--- src/test/java/ee/taltech/volatilator/controller/VolatilityControllerMockMvcUnhandledErrorTest.java
+++ src/test/java/ee/taltech/volatilator/controller/VolatilityControllerMockMvcUnhandledErrorTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class VolatilatorControllerMockMvcUnhandledErrorTest {
+public class VolatilityControllerMockMvcUnhandledErrorTest {
 
     @Autowired
     private MockMvc mvc;
