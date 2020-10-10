91cd97bcc80bc055d526231a6b2773b124d8ed0a
Giorgi Zeikidze <gizeik@ttu.ee>
2020-10-10 18:35:28 +0300

    I answered

--- src/main/java/a_theory/question6/Tester3.java
+++ src/main/java/a_theory/question6/Tester3.java
@@ -8,7 +8,22 @@ public class Tester3 {
 
     //todo p1
     // First. Write the definition and explain what is it.
+    // performance testing
+    /*
+    >performance testing is testing methodology where rather than testing specific
+    functionality of the application overall performance is tested, usually in terms
+    of time and efficiency
+    >point of these tests is to make user experience better and/or locate bottlenecks
+     */
 
     //todo p2
     // Second. Write the definition and explain what is it.
+    // load testing
+    /*
+    >Load testing is part of performance testing, it focuses on behaviour under
+    normal and peak load conditions
+    >Point is to estimate performance is such conditions making sure application
+    remains to be usable
+    >It can also highlight bottlenecks and help estimate capacity of setup
+     */
 }
