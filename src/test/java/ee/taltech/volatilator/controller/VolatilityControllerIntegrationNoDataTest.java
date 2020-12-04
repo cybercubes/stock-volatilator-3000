c86d2b992839f7f94897dba1627dcf2f7a767bb4
Archil Kristinashvili <arkris@ttu.ee>
2020-12-04 18:13:33 +0000

    Update VolatilityControllerIntegrationNoDataTest.java

--- src/test/java/ee/taltech/volatilator/controller/VolatilityControllerIntegrationNoDataTest.java
+++ src/test/java/ee/taltech/volatilator/controller/VolatilityControllerIntegrationNoDataTest.java
@@ -1,7 +1,7 @@
 package ee.taltech.volatilator.controller;
 
 
-import org.junit.jupiter.api.Test;
+/*import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -16,10 +16,10 @@ class VolatilityControllerIntegrationNoDataTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
-    /*@Test
+    @Test
     void getDataDefault() {
         assertThat(((LinkedHashMap) this.restTemplate.getForObject("/volatilator?endDate=1851-10-03&startDate=1851-09-06&symbol=FB", LinkedHashMap.class).get("body")).get("message")).isEqualTo("No data entries found.");
 
-    }*/
+    }
 
-}
+}*/
