00fe3e85cc2358e989f893415bc38502aa705375
Kirill Lõssenko <cyberandcubes@gmail.com>
2020-12-21 01:44:10 +0200

    fixed tests

--- src/test/java/ee/taltech/volatilator/controller/IndexControllerTest.java
+++ src/test/java/ee/taltech/volatilator/controller/IndexControllerTest.java
@@ -15,7 +15,7 @@ public class IndexControllerTest {
 
     @Test
     public void greetingShouldReturnDefaultMessage() {
-        assertThat(this.restTemplate.getForObject("/", String.class)).isEqualTo("API is up");
+        assertThat(this.restTemplate.getForObject("/", String.class)).isEqualTo("API is up, baby!");
     }
 
 }
