1d40a17e2202d53bee285eb74b74fd876786c19c
Giorgi Zeikidze <gizeik@ttu.ee>
2020-12-22 16:09:41 +0200

    Theory CI/CD

--- src/main/java/c_theory/question11/CI.java
+++ src/main/java/c_theory/question11/CI.java
@@ -4,28 +4,30 @@ public class CI {
 
     //todo A
     // What is integration?
-    // Answer:
+    // Answer: Integration is a process of merging new part of code to main code
 
     //todo B
     // What makes integration continuous?
-    // Answer:
+    // Answer: C part in CI comes from integration being done frequently,
+    // usually as soon as we have some kind of working code
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick [x] which ones stand for continuous integration
-    // [] compile
-    // [] test
+    // [x] compile
+    // [x] test
     // [] deploy
 
     //todo D
     // Name and explain 1 benefit of CI
-    // 1
+    // 1 it's faster in detecting problems, detecting problems fast makes them easier to solve
 
     //todo E
     // Name and explain 1 drawback of CI
-    // 1
+    // 1 Setting good CI up is hard, it takes effort and time so it's not an universal solution or a must
+    // depending on size of a team/project it might not be worth it
 
     //todo F
     // In the class we are using gitlab-ci. Name 2 alternatives to it
-    // 1
-    // 2
+    // 1 Jenkins
+    // 2 CircleCI
 }
--- src/main/java/c_theory/question12/CD.java
+++ src/main/java/c_theory/question12/CD.java
@@ -4,28 +4,33 @@ public class CD {
 
     //todo A
     // What is delivery?
-    // Answer:
+    // Answer: Delivery of software is to get software to such state that it's usable for user
+    // this could include delivering new product or new version of existing one
 
     //todo B
     // What makes delivery continuous?
-    // Answer:
+    // Answer: Process is incremental that is as soon as we have new feature added
+    // it is seamlessly delivered to end user.
 
     //todo C
     // In the class we had 3 stages in our CI/CD process, tick which ones stand for continuous delivery
     // [] compile
     // [] test
-    // [] deploy
+    // [x] deploy
 
     //todo D
     // Name and explain 1 benefit of CD
-    // 1
+    // 1 Faster deployment, traditional releases take more time, more effort and cost way more
+    // compared to that CD makes deployment is effortless
 
     //todo E
     // Name and explain 1 drawback of CD
-    // 1
+    // 1 Easy to break things
+    // If CI part is not done well and QA is no good CD will make users experience miserable
+    // continuously delivering broken product
 
     //todo F
     // Which one is more important to start with CI or CD? Why?
-    // CI/CD:
-    // Because:
+    // CI/CD: CI
+    // Because: Bad CI or no CI guarantee bad CD
 }
