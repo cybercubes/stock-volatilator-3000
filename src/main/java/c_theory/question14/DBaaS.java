bf4adcac0871183fb8c072a481ec3fa40b959e4b
Archil Kristinashvili <arkris@ttu.ee>
2020-12-22 17:21:45 +0000

    Answered DBaaS questions

--- src/main/java/c_theory/question14/DBaaS.java
+++ src/main/java/c_theory/question14/DBaaS.java
@@ -4,30 +4,30 @@ public class DBaaS {
 
     //todo A
     // What is DBaaS (Database as a service)?
-    // Answer:
+    // Answer: DBaaS is basically database server provided by cloud, using it will eliminate need to set up your own database.
 
     //todo B
     // Name 2 examples of DBaaS.
-    // 1
-    // 2
+    // 1 Amazon RDS
+    // 2 MongoDB Atlas 
 
     //todo C
     // Name and explain 2 benefits of using DBaaS
-    // 1
-    // 2
+    // 1 It saves a lot of time for sysadmins/devops.
+    // 2 It is more secure, scalable and has provisioning, which saves money.
 
     //todo D
     // Name and explain 1 drawback of using DBaaS
-    // 1
+    // 1 No direct access
 
     //todo E
     // What other -aaS do you know?
-    // 1
-    // 2
-    // 3
+    // 1 Payments
+    // 2 Infrastructure
+    // 3 Code
 
     //todo F
     // In your opinion which aaS are the most useful and should be integrated first.
-    // Answer:
+    // Answer: Infrastructure as a service
 
 }
